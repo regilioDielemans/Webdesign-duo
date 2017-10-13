cc7c8b1f3267a2d75457411cadb540a4eace0821
regilioDielemans <regilio_dielemans@hotmail.com>
2017-10-13 13:42:11 +0200

    Hoofdleters werken ook

--- js/javascript.js
+++ js/javascript.js
@@ -2,7 +2,7 @@
 function WelkomFunction() {
     var Name = document.getElementById("Name").value;
     
-    if(Name == "frank tempelman"){
+    if(Name == "frank tempelman" || Name == "Frank tempelman" || Name == "frank Tempelman" || Name == "Frank Tempelman"){
     	document.getElementById("welkom").innerHTML = "Welkom " + Name;
     	document.getElementById("welkom").style.fontFamily = "comic sans";
 
