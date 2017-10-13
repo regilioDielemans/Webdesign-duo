c9d29d5cb2b99f0c1c2893777f9e97a413d50999
regilioDielemans <regilio_dielemans@hotmail.com>
2017-10-13 17:18:52 +0200

    tekst homepage

--- index.php
+++ index.php
@@ -3,9 +3,25 @@
 <div class="container">
 	<div class="jumbotron">
 	    <h1 id="welkom">Welkom</h1>		
-		  name: <input id="Name"type="text" name="fname" placeholder="Vul hier je naam in.">
+		  Naam: <input id="Name"type="text" name="fname" placeholder="Vul hier je naam in.">
 		  		<button onclick="WelkomFunction()">Verstuur</button>
 		
+	</div>
+	<div class="col-md-6">
+		<h3>Wat houd informatica in?</h3>
+		<p>
+			Informatica is een vak waarmee je van alles kan doen. Tegenwoordig is overal waar je kijkt wel iets wat. Even Spotify streamen, op facebook een status delen, de verwarming hoger zetten met je telefoon en zo nog veel meer. Ben jij altijd bezig met de nieuwste technologieën? Dan is informatica misschien wel iets voor jou! 
+		</p>
+			<h3>Wat leer je op de opleiding?</h3>
+		<p>
+			Op de opleiding leer je alles wat met informatica  te maken heeft. je leert programmeren. In verschillende talen voor web, desktop/laptops en apps voor Android of iOS. Je leert alles wat erbij hoort vanaf het eerste gesprek met de klant. Tot het uitrollen van de applicatie/website en de terugkoppeling met de klant.
+		</p>
+	</div>
+	<div class="col-md-6">
+		<h3>Welke richtingen kan je in?</h3>
+		<p>
+			Je kan met deze opleiding veel richtingen in. Bijvoorbeeld Softwareontwikkelaar dan ben je vooral bezig met het ontwerpen, programmeren en testen van applicaties en apps. Ook kan je IT-architect worden. Dit is geen startersfunctie, maar een doorgroeifunctie. Je initieert en bewaakt de cohesie van IT-systemen en ontwikkelingen en legt voortdurend de relatie tot de business- en organisatiedoelen. Daarnaast kan je Projectmanager in de IT worden. Je geeft dan leiding aan een groep werknemers die werkzaam zijn in de IT-afdeling van een bedrijf. Je kan ook nog IT-consultent worden. Een IT- of ICT-consultant adviseert, meestal in dienst van een IT-bedrijf, klanten over IT of ICT.
+		</p>
 	</div>
 </div>
 							
