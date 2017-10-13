356f3acb5e7f02a4406e4c3ca7f32093a27afecb
regilioDielemans <regilio_dielemans@hotmail.com>
2017-10-13 17:57:35 +0200

    contact pagina toegevoegd

--- contact.php
+++ contact.php
@@ -2,38 +2,32 @@
 <div class="container">
 	<h1>Contact</h1>
 
+	<div class="col-md-6">
+	<h4>website gemaakt door:</h3>
 	<p>
-		•	Maak in het navigatiemenu duidelijk zichtbaar een menu-item ‘Contact’. Dit item is een link naar een pagina waar je vermeldt:<br>
-			o	Wie de makers van de site zijn, met naam, studentnummer, emailadres en eventueel groeps-nummer van de makers;<br>
-			o	Voor welke eindopdracht de site gemaakt is;<br>
-			o	Dat deze opdracht is gemaakt in het kader van de opleiding Informatica.
-
+		Regilio Dielemans <br />
+		Rdielema2@avans.nl <br />
+		2114221 <br />
 	</p>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	<p>
+		Damian Buzink <br />
+		Dmbuzink@avans.nl <br />
+		2123162 <br />
+		
+	</p>	
+	</div>
+	<div class="col-md-6">
+		<h4>Webdesign:</h4>
+		<p>Deze website is gemaakt als eindopdracht voor webdesign.En is in het kader gemaakt van informatica.</p>
+	</div>
 </div>
+	
+<iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d619.7612433500102!2d4.792745203041075!3d51.58573912401532!3m2!1i1024!2i768!4f13.1!5e0!3m2!1snl!2snl!4v1507909022011" width="100%" height="500px" frameborder="0" allowfullscreen></iframe>
+	
+
+
+
+
+
+
 <?php include 'includes/footer.php'; ?>
