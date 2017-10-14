2576d29ca2ba6693d926e371b3e26512cd8cdc70
dmbuzink <dmbuzink@avans.nl>
2017-10-14 16:14:07 +0200

    pagina geschiktheid toegevoegd

--- geschiktheid.php
+++ geschiktheid.php
@@ -1,35 +1,80 @@
 <?php include 'includes/header.php'; ?>
 <div class="container">
 	<h1>Geschiktheid</h1>
-	<div class="col-md-6 col-xs-12">
-		<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi eum ad repudiandae nostrum ut quaerat maiores dolorem similique consectetur magni itaque soluta, exercitationem quidem adipisci et harum maxime rerum distinctio! </p>
+	<div class="col-lg-6 col-md-6 col-xs-12" >
+		<p> In een auto of in een smartphone. Overal zul je software vinden en voor jou is dit geen probleem. Je kan je weg altijd vinden met software en wanneer er dan weer eens iets mis gaat bij iemand, wordt jij aangekeken als degene die het kan oplossen. Dit is de afkomst van bijna elke informaticus. Wil jij leren hoe je software ontwikkelt en alles daar om heen? Als jouw antwoord ja is, dan ben je een geschikte kandidaat voor informatica </p>
+
+		<p>Als informaticus wordt er van je verwacht dat je een aantal compenties in huis hebt. Dat zijn de volgende competenties: analytisch, accuratesse, logisch, samenwerken, nieuwsgierigheid, discipline, technisch inzicht, communicatief vaardig, conceptueel denken.</p>
+
+		<p>Als informaticus zal je ook je hele carrière blijven leren. De technische wereld ontwikkelt zich ongelooflijk snel en jij zal dat allemaal bij moeten houden. Daarnaast wordt er ook verwacht dat je in het algemeen leert. Misschien zal je bij een bepaald project een techniek moet gebruiken die je nog nooit eerder gebruikt hebt. Je zal dan daar mee om moeten kunnen gaan. </p>
 	</div>
-	
+
+	<div class="col-lg-6 col-md-6 col-xs-12">
+		<a target="_blank" href="https://www.pexels.com/photo/adult-business-businessman-close-up-428339/">
+			<img class="img-responsive" id="personPic" src="images/personPic.jpeg">
+			<p><br></p>
+		</a>
+	</div>
+
+	<div class="container" class="col-lg-6 col-md-6 col-xs-12">
+		<h2>Toelatingseisen</h2>
+		<p>Als je aan deze opleiding wilt beginnen zul je moeten voldaan aan de volgende toelatingseisen</p>
+		<table class="table table-hover col-lg-12 col-md-12 col-xs-12">
+			<thread>
+				<tr>
+					<th class="col-lg-6 col-xs-6 col-md-6">Profiel</th>
+					<th class="col-lg-6 col-xs-6 col-md-6">Toelichting</th>
+				</tr>
+			</thread>
+			<tbody>
+				<tr>
+					<td>Havo C&#38;M</td>
+					<td>&#10003;</td>
+				</tr>
+				<tr>
+					<td>Havo E&#38;M</td>
+					<td>&#10003;</td>
+				</tr>
+				<tr>
+					<td>Havo N&#38;G</td>
+					<td>&#10003;</td>
+				</tr>
+				<tr>
+					<td>Havo N&#38;T</td>
+					<td>&#10003;</td>
+				</tr>
+				<tr>
+					<td>Vwo C&#38;M</td>
+					<td>&#10003;</td>
+				</tr>
+				<tr>
+					<td>Vwo E&#38;M</td>
+					<td>&#10003;</td>
+				</tr>
+				<tr>
+					<td>Vwo N&#38;G</td>
+					<td>&#10003;</td>
+				</tr>
+				<tr>
+					<td>Vwo N&#38;T</td>
+					<td>&#10003;</td>
+				</tr>
+				<tr>
+					<td>Mbo-niveau 4</td>
+					<td>Alleen als je mbo-opleiding voldoende aansluit, ben je automatisch toelaatbaar voor deze opleiding. Klik op: <a href="http://www.avans.nl/studeren/aanmelden/toelating/doorstroom-mbo-hbo" target="_blank">lees meer</a>, als je meer wilt weten over het doorstromen van de mbo naar de hbo.</td>
+				</tr>
+				<tr>
+					<td>Hbo-propedeuse</td>
+					<td>&#10003;</td>
+				</tr>
+				<tr>
+					<td>Vwo-propedeuse</td>
+					<td>&#10003;</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
