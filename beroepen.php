416fbdf00677fb8ab0fc3202ac30885e78b71b69
dmbuzink <dmbuzink@avans.nl>
2017-10-14 16:54:59 +0200

    pagina "beroepen" ingevult

--- beroepen.php
+++ beroepen.php
@@ -2,8 +2,24 @@
 <div class="container">
 	<h1>Beroepen</h1>
 	<div class="col-md-6 col-xs-12">
-		<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi eum ad repudiandae nostrum ut quaerat maiores dolorem similique consectetur magni itaque soluta, exercitationem quidem adipisci et harum maxime rerum distinctio! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti libero a soluta. Illum neque id labore, iusto blanditiis accusantium nobis. Sunt aspernatur quam, voluptatem esse omnis, exercitationem delectus libero quasi!</p>
+		<p>Na het afronden van de opleiding informatica heb je veel beroepsmogelijkheden die weer veel variatie hebben. Dat komt dan ook nog is met een zeer bloeiende sector, waardoor een baan vinden relatief gemakkelijk is. Dit is door het grote tekort aan informatici. </p>
+		
+		<p>Zo kan je natuurlijk software ontwikkelaar worden. Dit is het beroep waar de meeste informatici belanden. Het hart van de opleiding informatica, software ontwikkelen. Hier werk je in een team met mensen aan projecten. Bij het ene bedrijf heb je veel collega's, een software architect, scrum master, team leader, QA tester. En bij het andere bedrijf zullen dingen als QA testing een van jouw taken zijn. </p>
+
+		<p>Zo kan je bijvoorbeeld ook ICT-consultant worden. Hierbij zal je jouw uitgebeide ICT-kennis of specialisatie gebruiken om te adviseren en te ondersteunen. Hier zal je communicatief extra vaardig moeten zijn maar dan weer wel veel variatie. Ook waarschijnlijk wel iets voor die super nieuwsgierige mensen.</p>
+
+		<p>Na een aantal jaar ervaring opgedaan te hebben kan je ook een, al eerder genoemde, software architect worden. Hier neem je de leiding in het design van de software en zal je het gehele systeem moeten bedenken. Je moet dus het systeem uit kaarten en ook zal je veel bezig zijn met het vertalen van concepten en ideeën naar concrete software-oplossingen.</p>
+
+		<p>Natuurlijk zijn dit maar enkele van de vele beroepen waar de deur, na het voltooien van de opleiding informatica, voor je open zult gaan. Dit zijn dan ook maar enkele prominente voorbeelden. Ook kan je je natuurlijk specialiseren in bijvoorbeeld het creeën van kunstmatige intelligentie (A.I.) of data science. </p>
 	</div>
+
+	<div class="col-lg-6 col-md-6 col-xs-12">
+		<a target="_blank" href="https://www.pexels.com/photo/adult-apple-device-business-code-340152/">
+			<img class="img-responsive" id="personPic" src="images/personPic2.jpeg">
+			<p><br></p>
+		</a>
+	</div>
+
 	
 </div>
 
